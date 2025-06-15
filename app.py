0611027085c2b541b99fdf2b4ef06c65ecaef384
niki2805 <n.masanagi28@gmail.com>
2025-06-14 21:10:34 -0700

    changes

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello from Flask on OpenShift 🚀"
+@app.route('/info')
+def info():
+    db_url = os.getenv('DB_URL', 'Not set')
+    jwt_secret = os.getenv('JWT_SECRET', 'Not set')
+    return jsonify({
+        "db_url": db_url,
+        "jwt_secret": jwt_secret
+    })
 
-# OpenShift routes to port 8080 by default
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
